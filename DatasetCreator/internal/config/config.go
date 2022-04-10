ed44e000509f3bf16061574c352539a16ca7d868
v.kochkin <v.kochkin@collabox.dev>
2022-04-10 16:05:58 +0300

    Made slowly dataset creator

---
+++ DatasetCreator/internal/config/config.go
@@ -0,0 +1,9 @@
+package config
+
+var (
+	postsFilePath = "../Dataset/posts.csv"
+	tagsFilePath  = "../Dataset/tags.csv"
+	sheetName     = "posts_length"
+	xlsxFilePath  = "data/posts_length_test.xlsx"
+	titles        = []string{"id", "post_id", "title", "text", "tags"}
+)
